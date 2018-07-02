c36b14b6952d1b1d2338e73c61e6c96b9ab8bfbc
Travis Roach <travis.roach24@gmail.com>
2018-07-02 16:41:18 -0600

    end to end testing

--- cypress/integration/tests/test.js
+++ cypress/integration/tests/test.js
@@ -2,31 +2,48 @@
 describe('Bridge The Fridge', function () {
   it('.should() - assert that <title> is correct', function () {
     cy.visit('http://localhost:8080/')
+
+    // review home/landing page
     cy.get('#app > nav').children('a', 'div').should('exist')
     cy.get('#app > nav > a').should("have.attr", "href", `/`)
     cy.get('#app > nav > div > a').should("have.attr", "href", `/#/cart`)
-    cy.get('#app > div > div').children('h1').should('have.text', 'Welcome!')
+    cy.get('#app > div > div').children('h1').should('have.text', 'Shopping is Eazy!')
     cy.get('#app > div > div > p:nth-child(2)').should('exist')
     cy.get('#app > div > div > p:nth-child(4)')
       .children().eq(0).should("have.attr", "href", `#/inventory/buy`)
     cy.get('#app > div > div > p:nth-child(4)')
       .children().eq(1).should("have.attr", "href", `#/inventory/sell`)
+    
 
-    // cy.title().should('include', 'Bridge The Fridge')
-    // cy.get('header img').should("have.attr", "src", `/static/bridge-the-fridge-logo.png`)
-    // cy.get("#top > form > label").should('have.text', "Add Ingredients (separated by commas)")
-    // cy.get("#search-bar > input").should("be.empty")
-    // cy.get("#search-bar > input").type('rice, beans, chicken')
-    // cy.get("#search-bar > button").click()
-    // cy.get("#recipe-container > li").should("have.length.above", 7)
-    // cy.get("#recipe-container > li > div.card > div > div").eq(0).children()
-    //   .eq(0).children('img','h4').should('exist')
-    // cy.get("#recipe-container > li > div.card > div > div").eq(0).children()
-    //   .eq(1).children('img','p').should('exist')
-    // cy.get('#recipe-container > li > div.card').eq(0).click()
-    // cy.get('#modal-center1___BV_modal_header_').children('h5').should('exist')
-    // cy.get('body').children('img', 'h6').should('exist')
-    // cy.get('body > ul').should("have.length.above", 2)
-    // cy.get('#modal-center1___BV_modal_header_ > button').eq(0).click()
+    // simuate posting an item to sell
+    cy.get('#app > div > div > p:nth-child(4)').children().eq(1)
+      .click()
+    cy.get('#app > div > div > ul')
+      .children().should("have.length.above", 1)
+    cy.get('#app > div > div > p:nth-child(3) > a').click()
+    cy.get('#inputDefault').type('Beach Ball')
+    cy.get('#price-input > div > input').type('4')
+    cy.get('#exampleTextarea').type('Summertime necessity')
+    cy.get('#exampleSelect1').select('3')
+    cy.get('input').eq(2).type('https://images-na.ssl-images-amazon.com/images/I/71%2ByQUGOR4L._SL1491_.jpg')
+    cy.get('#submit-button').click()
+
+    // edit/delete inventory item
+    cy.get('#item-container > li > div > div.card-bottom > div > button.btn.btn-secondary').eq(0).click()
+    cy.get('#price-input > div > input').type('{backspace}').type('7')
+    cy.get('#submit-button').click()
+
+    cy.get('#item-container > li > div > div.card-bottom > div > button.btn.btn-danger').eq(0).click()
+    cy.get('#app > nav > a').click()
+
+    // simulate buying an item
+    cy.get('#app > div > div > p:nth-child(4)').children().eq(0)
+      .click()
+    cy.get('#app > div > div > ul')
+      .children().should("have.length.above", 1)
+    cy.get('#item-container > li > div > div.card-bottom > button').eq(0).click()
+    cy.get('#navbarColor01 > a').click()
+    cy.get('#app > div > div > div > button').click()
+    cy.get('iframe').should('exist')
   })
 })
